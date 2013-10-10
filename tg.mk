ca254a70e2fd75a45b5a3ac41bc121302a6dc1ff
hedwig34 <hedwig34@gmail.com>
2013-10-10 14:06:43 -0400

    Initial changes for Gummy.

--- tg.mk
+++ tg.mk
@@ -1,16 +1,16 @@
 $(call inherit-product, device/samsung/d2vzw/full_d2vzw.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/cdma.mk)
+$(call inherit-product, vendor/Gummy/config/cdma.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/Gummy/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/Gummy/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2vzw TARGET_DEVICE=d2vzw BUILD_FINGERPRINT="Verizon/d2vzw/d2vzw:4.1.2/JZO54K/I535VRBMF1:user/release-keys" PRIVATE_BUILD_DESC="d2vzw-user 4.1.2 JZO54K I535VRBMF1 release-keys"
 
-PRODUCT_NAME := cm_d2vzw
+PRODUCT_NAME := tg_d2vzw
 PRODUCT_DEVICE := d2vzw
 
